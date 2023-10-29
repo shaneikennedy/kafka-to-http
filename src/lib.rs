be1e9662fe6cb094e3dc376f5ade306eceaef47a
Shane Kennedy <shane.kennedy19@gmail.com>
2023-10-29 14:33:33 +0100

    Encap logic in consumer wrapper and message handler
    This also involved changing threadpool libs in order to have scoped
    threads such that I could use injected handler methods in the consumer wrapper

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
+use rayon::ThreadPool;
+use reqwest::blocking::{Client, Response};
 use serde::Deserialize;
 use serde_yaml::{self};
+use std::time::Duration;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
@@ -30,5 +34,104 @@ impl Config {
         let f = std::fs::File::open(config_file).expect("Could not open file.");
         let config: Config = serde_yaml::from_reader(f).expect("Could not read values.");
         Ok(config)
+    }
+}
+
+pub struct ConsumerWrapper {
+    pub consumer: Consumer,
+    pub worker_pool: ThreadPool,
+    pub handler: MessageHandler,
+}
+
+impl ConsumerWrapper {
+    pub fn build(
+        config: ConsumerConfig,
+        message_handler: MessageHandler,
+    ) -> Result<ConsumerWrapper, &'static str> {
+        let consumer = Consumer::from_hosts(vec![config.host])
+            .with_topic(config.topic)
+            .with_fallback_offset(FetchOffset::Latest)
+            .with_group(config.app_name.to_owned())
+            .with_offset_storage(GroupOffsetStorage::Kafka)
+            .create()
+            .unwrap();
+
+        let worker_pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(8)
+            .build()
+            .unwrap();
+
+        return Ok(ConsumerWrapper {
+            consumer,
+            worker_pool,
+            handler: message_handler,
+        });
+    }
+
+    pub fn consume(&mut self) {
+        loop {
+            self.worker_pool.scope(|s| {
+                for ms in self.consumer.poll().unwrap().iter() {
+                    for m in ms.messages() {
+                        let data = Vec::from(m.value);
+                        s.spawn(|_| self.handler.handle(data));
+                    }
+                    let _ = self.consumer.consume_messageset(ms);
+                }
+                self.consumer.commit_consumed().unwrap();
+            })
+        }
+    }
+}
+
+pub struct MessageHandler {
+    client: Client,
+    msg_destination: String,
+    timeout_policy: Duration,
+}
+
+impl MessageHandler {
+    pub fn build(http_config: HttpConfig) -> MessageHandler {
+        MessageHandler {
+            client: reqwest::blocking::Client::new(),
+            msg_destination: format!("{0}/{1}", http_config.host, http_config.endpoint),
+            timeout_policy: Duration::new(http_config.timeout, 0),
+        }
+    }
+
+    pub fn handle(&self, data: Vec<u8>) {
+        let request_defaults = self.setup_http_request();
+        let body = String::from_utf8(data.to_vec()).expect("expecting string body");
+        println!("{:#?}", body);
+        let request = request_defaults.body(body).send();
+        if request.is_ok() {
+            let resp = request.ok();
+            match resp {
+                Some(r) => self.handle_response(r),
+                None => self.handle_request_error(),
+            }
+        } else {
+            self.handle_request_error();
+        }
+    }
+
+    fn setup_http_request(&self) -> reqwest::blocking::RequestBuilder {
+        self.client
+            .post(self.msg_destination.clone())
+            .timeout(self.timeout_policy)
+    }
+
+    fn handle_request_error(&self) {
+        println!("what the fuck");
+    }
+
+    fn handle_response(&self, response: Response) {
+        print!("Received {:?} ", response.status());
+        match response.status().as_u16() {
+            s if s >= 200 && s < 400 => println!("Message delivered successfully"),
+            s if s >= 400 && s < 500 => println!("Client error, check your request"),
+            s if s >= 500 => println!("Server error"),
+            _ => println!("received unhanlded status code"),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use std::{env, process, time::Duration};
+use std::{env, process};
 
-use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
-use kafka_quickstart::{Config, ConsumerConfig, HttpConfig};
-use reqwest::blocking::{Client, RequestBuilder, Response};
-use threadpool;
+use kafka_quickstart::{Config, ConsumerWrapper, MessageHandler};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,63 +9,7 @@ fn main() {
         process::exit(1);
     });
 
-    let mut consumer = init_consumer(config.consumer);
-    let client = reqwest::blocking::Client::new();
-    let threadpool = threadpool::Builder::new().num_threads(10).build();
-    loop {
-        for ms in consumer.poll().unwrap().iter() {
-            for m in ms.messages() {
-                let request_defaults = setup_http_request(&client, &config.http);
-                let body = String::from_utf8(m.value.to_vec()).expect("expecting string body");
-                println!("{:#?}", body);
-                threadpool.execute(|| handle_message(request_defaults, body));
-            }
-            let _ = consumer.consume_messageset(ms);
-        }
-        consumer.commit_consumed().unwrap();
-    }
-}
-
-fn init_consumer(config: ConsumerConfig) -> Consumer {
-    Consumer::from_hosts(vec![config.host])
-        .with_topic(config.topic)
-        .with_fallback_offset(FetchOffset::Latest)
-        .with_group(config.app_name.to_owned())
-        .with_offset_storage(GroupOffsetStorage::Kafka)
-        .create()
-        .unwrap()
-}
-
-fn handle_message(req: RequestBuilder, body: String) {
-    let request = req.body(body).send();
-    if request.is_ok() {
-        let resp = request.ok();
-        match resp {
-            Some(r) => handle_response(r),
-            None => handle_request_error(),
-        }
-    } else {
-        handle_request_error();
-    }
-}
-
-fn setup_http_request(client: &Client, config: &HttpConfig) -> reqwest::blocking::RequestBuilder {
-    let msg_dst = format!("{0}/{1}", config.host, config.endpoint);
-    client
-        .post(msg_dst)
-        .timeout(Duration::new(config.timeout, 0))
-}
-
-fn handle_request_error() {
-    println!("what the fuck");
-}
-
-fn handle_response(response: Response) {
-    print!("Received {:?} ", response.status());
-    match response.status().as_u16() {
-        s if s >= 200 && s < 400 => println!("Message delivered successfully"),
-        s if s >= 400 && s < 500 => println!("Client error, check your request"),
-        s if s >= 500 => println!("Server error"),
-        _ => println!("received unhanlded status code"),
-    }
+    let message_handler = MessageHandler::build(config.http);
+    let mut consumer = ConsumerWrapper::build(config.consumer, message_handler).unwrap();
+    consumer.consume();
 }
